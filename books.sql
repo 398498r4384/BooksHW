9310be567c3dd312cf9286cbb421838268f70ba2
Aya Khaled <87143218+398498r4384@users.noreply.github.com>
2022-07-23 22:50:00 +0200

    Add files via upload

---
+++ books.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 23 يوليو 2022 الساعة 22:24
+-- إصدار الخادم: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flaskdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(2) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `count` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `books`
+--
+
+INSERT INTO `books` (`id`, `name`, `count`) VALUES
+(1, 'How to get a good grade in DOS in 40 minutes a day', 15),
+(2, 'RPCs for Noobs', 20),
+(3, 'Xen and the Art of Surviving Undergraduate School', 35),
+(4, 'Cooking for the Impatient Undergrad', 14),
+(5, 'Temp Book', 22);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
